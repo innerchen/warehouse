1824672ab69462fc36b69c79d3de46cf1f831aa7
innerchen <innerchen@outlook.com>
2017-10-17 15:47:33 +0800

    update ware.sh

--- ware.sh
+++ ware.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/innerchen/warehouse.git ~/.warehouse
+! [ -e ~/.warehouse ] && git clone https://github.com/innerchen/warehouse.git ~/.warehouse
 python3 ~/.warehouse/ware.py ~/.warehouse/ware.json
+
+echo Please compile YouCompleteMe manually.
+
