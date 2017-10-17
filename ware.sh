ffe0136515420260387d5a4d44c3c8c6b3f68514
innerchen <innerchen@outlook.com>
2017-10-17 15:38:36 +0800

    update ware.sh

--- ware.sh
+++ ware.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/innerchen/warehouse.git ~/.warehouse
-python3 ~/.warehouse/ware.py ~/.warehouse/.ware.json
+python3 ~/.warehouse/ware.py ~/.warehouse/ware.json
