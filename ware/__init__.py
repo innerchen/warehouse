b71d8207e8d0486b25a6d002edc79aa3773ba4fa
innerchen <innerchen@outlook.com>
2017-10-18 18:01:37 +0800

    fix some bugs in ware

--- ware/__init__.py
+++ ware/__init__.py
@@ -1,3 +1,25 @@
 
 from ware.func import *
 from ware.ware import *
+
+home = os.path.expanduser("~")
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+temp_path = mkdir(os.path.join(tempfile(), "warehouse"))
+log = func.touch(os.path.join(temp_path, "ware.log"))
+
+func.echo("", log)
+os_type, os_version = func.uname()
+
+
+if os_type != "macOS" and os_type != "Ubuntu":
+    print("not supported system: ", os_type)
+    exit(-1)
+
+if os_type == "macOS" and func.which("brew") is None:
+    print("command not found: brew")
+    print("please install homebrew first")
+    exit(-1)
+
+password = func.passwd()
+
--- ware/ware.py
+++ ware/ware.py
@@ -1,13 +1,6 @@
 
-import os
-
-from ware import func
-from ware.func import command
+import ware
 from lib.termcolor import colored as _colored
-
-home = os.path.expanduser("~")
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os_type, os_version = func.uname()
 
 
 def colored(text, color=None, on_color=None, attrs=None):
@@ -15,40 +8,26 @@ def colored(text, color=None, on_color=None, attrs=None):
         attrs = [attrs]
     return _colored(text, color, on_color, attrs)
 
-if os_type != "macOS" and os_type != "Ubuntu":
-    print("not supported system: ", os_type)
-
-if os_type == "macOS" and func.which("brew") is None:
-    print("command not found: brew")
-    print("please install homebrew")
-    exit(-1)
-
-
-temp_path = func.mkdir(os.path.join(func.tempfile(), "warehouse"))
-log = func.touch(os.path.join(temp_path, "ware.log"))
-func.echo("", log)
-password = func.passwd()
-
 
 def install(program):
     print(colored("==>", color="blue"), colored("ware install", attrs="bold"), program)
-    if os_type == "Ubuntu":
-        command("apt install %s -y" % program, sudo=True)
-    if os_type == "macOS":
-        command("HOMEBREW_NO_AUTO_UPDATE=1 brew install %s" % program, output=True)
+    if ware.os_type == "Ubuntu":
+        ware.command("apt install %s -y" % program, sudo=True)
+    if ware.os_type == "macOS":
+        ware.command("HOMEBREW_NO_AUTO_UPDATE=1 brew install %s" % program, output=True)
 
 
 def update():
     print(colored("==>", color="blue"), colored("ware update", attrs="bold"))
-    if os_type == "Ubuntu":
-        command("apt update", sudo=True)
-    if os_type == "macOS":
-        command("brew update", output=True)
+    if ware.os_type == "Ubuntu":
+        ware.command("apt update", sudo=True)
+    if ware.os_type == "macOS":
+        ware.command("brew update", output=True)
 
 
 def upgrade(program=""):
     print(colored("==>", color="blue"), colored("ware upgrade", attrs="bold"), program)
-    if os_type == "Ubuntu":
-        command("apt upgrade %s -y" % program, sudo=True)
-    if os_type == "macOS":
-        command("brew upgrade %s" % program, output=True)
+    if ware.os_type == "Ubuntu":
+        ware.command("apt upgrade %s -y" % program, sudo=True)
+    if ware.os_type == "macOS":
+        ware.command("brew upgrade %s" % program, output=True)
